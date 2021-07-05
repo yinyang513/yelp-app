9537adffea10b1515fb9cbeb674653bc469481f1
yinyang513 <zyingyip@gmail.com>
2021-07-05 02:42:01 -0400

    added api call

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # yelp-app
 
-1. switch to yelp-env using . yelp-env/bin/activate
-2. pip install -r requirements.txt
-3. export FLASK_APP=app.py
-4. flask run
-
-access database shell: mongos
+1. switch to yelp-env using `. yelp-env/bin/activate`
+2. install all dependencies `pip install -r requirements.txt`
+3. `export FLASK_APP=app.py`
+4. run the backend `flask run`
+5. switch the frontend folder `cd yelp-app-frontend`
+6. run the frontend `npm start`
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 import requests
 import config
+import json
 # import flask_login
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/yelp-db"
 mongo = PyMongo(app)
 db = mongo.db.yelp
@@ -13,29 +16,51 @@ db = mongo.db.yelp
 def main_page():
     return 'Welcome to Yelp!'
 
-@app.route('/sign-in', methods = ['GET'])
+@app.route('/sign-in', methods = ['POST'])
 def sign_in():
-    user = request.form.get('username')
-    password = request.form.get('password')
-    credentials = db.find({"username": user, "password": password})
-    if ({"username": credentials["username"]} == user) and ({"password": credentials["password"]}):
-        return 'true' #go to home page with hello user
+    user = request.json['username']
+    password = request.json['password']
+    print(request.json['username'])
+    print(request.json['password'])
+    credentials = db.find_one({"username": user, "password": password})
+    print(credentials)
+    if (credentials == None):
+        # print('true')
+        return 'make account' #go to home page with hello user
+    elif credentials['username'] == user and credentials['password'] == password:
+        # print('true')
+        return 'True' #go to sorry try again
     else:
-        return 'false' #go to sorry try again
+        return 'False'
 
-@app.route('/register', methods = ['GET'])
+@app.route('/register', methods = ['POST'])
 def register():
-    first_name = request.form.get('first-name')
-    last_name = request.form.get('last-name')
-    email = request.form.get('email')
-    birthday = request.form.get('birthday')
-    hometown = request.form.get('hometown')
-    username = request.form.get('username')
-    password = request.form.get('password')
-    db.insert({"name": {"firstname": first_name, "lastname": last_name}, 
-                "email": email, "birthday": birthday, 
-                "hometown": hometown, "username": username, "password": password})
-    return #go to home page with hello user
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    birthday = request.json['birthday']
+    hometown = request.json['hometown']
+    username = request.json['username']
+    password = request.json['password']
+
+    print(request.json['first_name'])
+    print(request.json['last_name'])
+    print(request.json['email'])
+    print(request.json['birthday'])
+    print(request.json['hometown'])
+    print(request.json['username'])
+    print(request.json['password'])
+
+    temp = db.find_one({'email': email})
+    print(temp)
+
+    if temp == email:
+        return 'already registered'
+    else: 
+        db.insert({"name": {"firstname": first_name, "lastname": last_name}, 
+                    "email": email, "birthday": birthday, 
+                    "hometown": hometown, "username": username, "password": password})
+        return 'register' #go to home page with hello user
 
 @app.route('/profile', methods = ['POST'])
 def profile():
@@ -43,32 +68,64 @@ def profile():
     # db.find({"username": current_email})
     return 
 
-@app.route('/restaurants', methods = ['GET','POST'])
+@app.route('/restaurants', methods = ['POST'])
 def restaurants():
-    if request.method == 'GET':
-        restaurant = request.form.get('cuisine')
-        location = request.form.get('location')
-        radius = request.form.get('radius')
-        categories = request.form.get('categories')
-        sort_by = request.form.get('sort-by')
-        price = request.form.get('price')
-        open_now = request.form.get('open-now')
+    if request.method == 'POST':
+        ret = []
+        restaurant = request.json['restaurant']
+        location = request.json['location']
+        radius = request.json['radius']
+        categories = request.json['categories']
+        sort_by = request.json['sort_by']
+        price = request.json['price']
+        open_now = request.json['open_now']
 
-        url = "https://api.yelp.com/v3/businesses/search?term=" + str(restaurant) + "&location=" + str(location) + "&radius=" + str(radius) + "&categories=" + str(categories) + "&sort_by=" + str(sort_by) + " &price=" + str(price) + "&open_now=" + str(open_now)
+        # print(request.json['sort_by'])
+        # print(request.json['price'])
+        # print(request.json['open_now'])
 
-        headers = {
-        'Authorization': 'Bearer ' + config.api_key
-        }
+        url = "https://api.yelp.com/v3/businesses/search?term=" + str(restaurant) + "&location=" + str(location) + "&radius=" + str(radius) + "&categories=" + str(categories) + "&sort_by=" + str(sort_by) + "&price=" + str(price) + "&open_now=" + str(open_now)
 
+        headers = {'Authorization': 'Bearer ' + config.api_key}
         response = requests.request("GET", url, headers=headers)
 
-        print(response.text)
+        # print(json.loads(response.text))
+
+        # for restaurant in json.loads(response.text)["businesses"]:
+
+        #     db.insert({'name': restaurant['name'], 'image': restaurant['image_url'], 
+        #             'address': restaurant['location']['display_address'], 'phone': restaurant['display_phone'], 
+        #             'location': restaurant['coordinates']})
+
+        # restaurants = db.find()
+        # print(restaurants)
+            
+            # ret.append((restaurant['name'],restaurant['image_url'],restaurant['location']['display_address'],restaurant['display_phone'],restaurant['coordinates']))
+
+            # print(restaurant['name'])
+            # print(restaurant['image_url'])
+            # print(restaurant['location']['display_address'])
+            # print(restaurant['display_phone'])
+            # print(restaurant['coordinates'])
+            # print()
 
         #return the name, address, phone number, maybe the image, maybe the map location
 
-        return
-    else:
-        return
+        return json.loads(response.text)
+    # else:
+    #     allRestaurants = db.find()
+    #     ret = []
+    #     for restaurant in allRestaurants: 
+    #         ret.append(
+    #             {
+    #                 'name': restaurant['name']
+    #                 # 'image': restaurant['image_url'], 
+    #                 # 'address': restaurant['location']['display_address'], 
+    #                 # 'phone': restaurant['display_phone'], 
+    #                 # 'location': restaurant['coordinates']
+    #             })
+    #     return jsonify(tweets=ret)
+
 
 @app.route('/favorites', methods = ['GET','POST'])
 def favorites():
